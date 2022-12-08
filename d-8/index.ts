f7ac9b5585f8695e8b294d9dd9150c355d270198
Erik Thunberg <neme@whispered.se>
2022-12-08 16:05:12 +0100

    Day 8 - Better\?

--- d-8/index.ts
+++ d-8/index.ts
@@ -6,65 +6,48 @@ function createTreeGrid(trees: string): Array<Array<number>> {
     return grid;
 }
 
-function isVisible(grid: Array<Array<number>>, y: number, x: number): boolean {
-    const row = grid[y];
-    const column = grid.map((c) => c[x]);
-
-    if (Math.max(...row.slice(0, x)) < grid[y][x] || Math.max(...row.slice().reverse().slice(0, (row.length - 1) - x)) < grid[y][x] || Math.max(...column.slice(0, y)) < grid[y][x] || Math.max(...column.slice().reverse().slice(0, (column.length - 1) - y)) < grid[y][x]) {
-        return true;
-    }
-}
-
-function findNumVisibleTrees(grid: Array<Array<number>>): number {
+function findVisibleTrees(grid: Array<Array<number>>): number {
     let visible = 0;
 
     for (let y = 0; y < grid.length; y += 1) {
         for (let x = 0; x < grid[y].length; x += 1) {
-            visible += isVisible(grid, y, x) ? 1 : 0;
+            const row = grid[y];
+            const column = grid.map((c) => c[x]);
+            visible += (
+                row.slice(0, x).findIndex(n => n >= grid[y][x]) === -1
+                || row.slice().reverse().slice(0, (row.length - 1) - x).findIndex(n => n >= grid[y][x]) === -1
+                || column.slice(0, y).findIndex(n => n >= grid[y][x]) === -1
+                || column.slice().reverse().slice(0, (column.length - 1) - y).findIndex(n => n >= grid[y][x]) === -1
+            ) ? 1 : 0
         }
     }
 
     return visible;
 }
 
-function treeIsVisible(trees: Array<number>, height: number, fromHeight: number): Array<number> {
-    const highest = trees.length === -1 ? 0 : Math.max(...trees);
-
-    if (trees.length === 0) {
-        return [...trees, height];
-    }
-
-    if (highest >= fromHeight) {
-        return trees;
-    }
-
-    return [...trees, height];
-}
-
-function treeScore(grid: Array<Array<number>>, y: number, x: number): number {
-    const scores = [0, 0, 0, 0];
-
-    const row = grid[y];
-    const sumY = grid.map((c) => c[x]);
-
-    scores[0] = sumY.slice().reverse().slice(sumY.length - y).reduce((ns, n) => treeIsVisible(ns, n, grid[y][x]), []).length;
-    scores[1] = row.slice(0, x).reverse().reduce((ns, n) => treeIsVisible(ns, n, grid[y][x]), []).length;
-    scores[2] = sumY.slice(y + 1).reduce((ns, n) => treeIsVisible(ns, n, grid[y][x]), []).length;
-    scores[3] = row.slice(x + 1).reduce((ns, n) => treeIsVisible(ns, n, grid[y][x]), []).length;
-    return scores[0] * scores[1] * scores[2] * scores[3];
+function getTreeScore(numbers: Array<number>, height: number): number {
+    const index = numbers.findIndex((n) => n >= height);
+    return index === -1 ? numbers.length : index + 1;
 }
 
 function getHighestTreeScore(grid: Array<Array<number>>): number {
     const scores = [];
     for (let y = 0; y < grid.length; y += 1) {
         for (let x = 0; x < grid[y].length; x += 1) {
-                scores.push(treeScore(grid, y, x))
+            const row = grid[y];
+            const column = grid.map((c) => c[x]);
+            scores.push(
+                getTreeScore(column.slice().reverse().slice(column.length - y), grid[y][x]) *
+                getTreeScore(row.slice(0, x).reverse(), grid[y][x]) *
+                getTreeScore(column.slice(y + 1), grid[y][x]) *
+                getTreeScore(row.slice(x + 1), grid[y][x])
+            );
         }
     }
     return Math.max(...scores);
 }
 
-const sum1 = findNumVisibleTrees(createTreeGrid(trees));
+const sum1 = findVisibleTrees(createTreeGrid(trees));
 const sum2 = getHighestTreeScore(createTreeGrid(trees));
 
 console.log(`
